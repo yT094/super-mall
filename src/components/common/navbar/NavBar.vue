9778a3254da942dce37e3ceafc8f54714606abdf
ycs <1748780248@qq.com>
2022-05-01 11:43:07 +0800

    feat: encapsulate the navbar component

---
+++ src/components/common/navbar/NavBar.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="nav-bar">
+    <div class="left"><slot name="left"></slot></div>
+    <div class="center"><slot name="center"></slot></div>
+    <div class="right"><slot name="right"></slot></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NavBar",
+};
+</script>
+
+<style lang="less" scoped>
+.nav-bar {
+  display: flex;
+  // 下面三行代码，让文字处于盒子中心位置
+  height: 44px;
+  line-height: 44px;
+  text-align: center;
+  // 有平滑的阴影效果 
+  box-shadow: 0 1px 1px rgba(100, 100, 100, 0.2);
+  .left,
+  .right {
+    width: 44px;
+  }
+  .center {
+    flex: 1;
+  }
+}
+</style>
+
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,14 +1,40 @@
 <template>
-  <div>首页</div>
+  <nav-bar class="home-nav">
+    <div slot="center">购物街</div>
+  </nav-bar>
 </template>
 
 <script>
+import NavBar from "components/common/navbar/NavBar";
+
+import { getHomeMultidata } from "network/home";
+
 export default {
   name: "Home",
   components: {
+    NavBar,
+  },
+  data() {
+    return {
+      // result: null,
+      banners: [],
+      recommends: []
+    };
+  },
+  created() {
+    getHomeMultidata().then((res) => {
+      // console.log(res);
+      // this.result = res;
+      this.banners = res.data.banner.list;
+      this.recommends = res.data.recommend.list;
+    });
   },
 };
 </script>
 
 <style lang="less" scoped>
+.home-nav {
+  background-color: var(--color-tint);
+  color: #fff;
+}
 </style>
