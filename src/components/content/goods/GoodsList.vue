e97d5260e2d60835bd9819ed6fdf984ef3af009a
ycs <1748780248@qq.com>
2022-05-02 12:11:42 +0800

    feat: display goods

---
+++ src/components/content/goods/GoodsList.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="goods-list">
+    <goods-list-item v-for="item in goods" :key="item.id" :goods-item="item" />
+  </div>
+</template>
+
+<script>
+import GoodsListItem from "./GoodsListItem";
+
+export default {
+  name: "GoodsList",
+  props: {
+    goods: {
+      type: Array,
+      default() {
+        return [];
+      },
+    },
+  },
+  components: {
+    GoodsListItem,
+  },
+};
+</script>
+
+<style lang="less" scoped>
+.goods-list {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  padding: 2px;
+}
+</style>
